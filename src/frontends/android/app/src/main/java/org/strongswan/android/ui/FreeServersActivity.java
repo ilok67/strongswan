0f0f4854a2d61447b51f030b364f55cc0151d486
ilok67 <iloktest6@gmail.com>
2026-09-26 17:16:00 +0330

    Update FreeServersActivity.java

--- src/frontends/android/app/src/main/java/org/strongswan/android/ui/FreeServersActivity.java
+++ src/frontends/android/app/src/main/java/org/strongswan/android/ui/FreeServersActivity.java
@@ -142,7 +142,23 @@ public class FreeServersActivity extends AppCompatActivity
 			profile.setUsername(local.optString("eap_id", null));
 			profile.setPassword(local.optString("shared_secret", null));
 		}
-
+          JSONObject split = obj.optJSONObject("split-tunneling");
+if (split != null)
+{
+    int st = 0;
+    if (split.optBoolean("block-ipv4", false))
+    {
+        st |= VpnProfile.SPLIT_TUNNELING_BLOCK_IPV4;
+    }
+    if (split.optBoolean("block-ipv6", false))
+    {
+        st |= VpnProfile.SPLIT_TUNNELING_BLOCK_IPV6;
+    }
+    if (st != 0)
+    {
+        profile.setSplitTunneling(st);
+    }
+}
 		String certB64 = remote.optString("cert", null);
 		if (certB64 != null && !certB64.isEmpty())
 		{
